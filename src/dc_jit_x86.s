9bb8a18b0352380699602a2d89d6bae11b5a94d2
AlaskanEmily <emily@alaskanemily.net>
2018-08-19 02:06:12 -0700

    Optimize some of the output machine code

--- src/dc_jit_x86.s
+++ src/dc_jit_x86.s
@@ -89,36 +89,72 @@ push_zero:
     mov eax, 4
     ret
 
+dc_asm_immediate_zero:
+    ; Write:
+    ; xor eax, eax
+    ; push eax
+    ; movss XMM, [esp]
+    ; pop eax
+    mov [eax], DWORD 0xF350C031
+    shl cx, 11
+    or ecx, 0x0F100424
+    bswap ecx
+    mov [eax+4], ecx
+    mov [eax+8], BYTE 0x58
+    mov eax, 9
+    ret
+
+dc_asm_immediate_one:
+    ; write:
+    ; fld1
+    ; fstp [esp-4]
+    ; movss xmm0, [esp-4]
+    mov [eax], WORD 0xE8D9
+    mov [eax+2], DWORD 0xFC245CD9
+    shl cl, 3
+    add cl, 0x44
+    or ecx, 0xF30F1000
+    bswap ecx
+    mov [eax+6], ecx
+    mov [eax+10], WORD 0xFC24 
+    mov eax, 12
+    ret
+
 ; unsigned DC_ASM_WriteImmediate(void *dest, float value);
 _DC_ASM_WriteImmediate:
     mov eax, [esp+4] ; Get the dest
     mov edx, [esp+8] ; Get the immediate.
-    
-    ; TODO: FP zeroes could use xor eax, eax instead of a mov eax, IMM
-    
-    ; Write mov eax, IMM ; push eax ; mov xmm, [esp] ; pop eax
-    mov [eax], DWORD 0xB8 ; mov eax,
-    mov [eax+1], edx ; IMM for move
-    mov [eax+5], DWORD 0x50 ; push eax
     
     ; Get the current stack depth
     mov ecx, [dc_asm_index]
     inc ecx
     mov [dc_asm_index], ecx
     dec ecx
-; Translate the stack depth into the XMM register encoding
-    shl ecx, 3
-    or ecx, 4
     
-; Write the movss (0xF3 0x0F 0x10 MM 0x24)
-    mov [eax+6], BYTE 0xF3
-    mov [eax+7], BYTE 0x0F
-    mov [eax+8], BYTE 0x10
-    mov [eax+9], BYTE cl
-    mov [eax+10], BYTE 0x24
-; pop eax
-    mov [eax+11], BYTE 0x58
-    mov eax, 12
+    cmp edx, 0
+    je dc_asm_immediate_zero
+    
+    cmp edx, 0x3F800000
+    je dc_asm_immediate_one
+    
+    ; Write:
+    ; lea eax,[esp-0x4]
+    ; mov [eax], IMM
+    ; movss  xmm0, [eax]
+    ;
+    ; This is:
+    ; 0x24FC8D44 (lea eax,[esp-0x4])
+    ; 0xC700 IMM (mov [eax], IMM)
+    ; 0xF30F10 (XMM)
+    
+    mov [eax], DWORD 0xFC24448D
+    mov [eax+4], WORD 0x00C7
+    mov [eax+6], edx
+    shl cl, 3
+    or ecx, 0xF30F1000
+    bswap ecx
+    mov [eax+10], ecx
+    mov eax, 14
     ret
 
 ; unsigned DCJIT_CDECL DC_ASM_WriteAdd(void *dest);
@@ -131,20 +167,21 @@ _DC_ASM_WriteSub:
     mov ecx, 0xF30F5C00
     jmp dc_asm_write_arithmetic
 
-; unsigned DCJIT_CDECL DC_ASM_WriteMul(void *dest);
-_DC_ASM_WriteMul:
-    mov ecx, 0xF30F5900
-    jmp dc_asm_write_arithmetic
-
 ; unsigned DCJIT_CDECL DC_ASM_WriteDiv(void *dest);
 _DC_ASM_WriteDiv:
     mov ecx, 0xF30F5E00
+    jmp dc_asm_write_arithmetic
+
+; unsigned DCJIT_CDECL DC_ASM_WriteMul(void *dest);
+; Mul is last, since it's the most likely and we can avoid a jmp.
+_DC_ASM_WriteMul:
+    mov ecx, 0xF30F5900
     ; jmp dc_asm_write_arithmetic
 
 dc_asm_write_arithmetic:
     mov eax, [dc_asm_index]
     dec eax
-    mov cl, [dc_asm_arithmetic_codes + eax - 1]
+    xor cl, [dc_asm_arithmetic_codes + eax - 1]
     mov [dc_asm_index], eax
     bswap ecx
     mov edx, [esp+4]
@@ -188,7 +225,7 @@ section .data
     
     DC_ASM_jmp_size: dd 6
     DC_ASM_push_arg_size: dd 5
-    DC_ASM_immediate_size: dd 12
+    DC_ASM_immediate_size: dd 14
     DC_ASM_sub_size: ; FALLTHROUGH
     DC_ASM_mul_size: ; FALLTHROUGH
     DC_ASM_div_size: ; FALLTHROUGH
