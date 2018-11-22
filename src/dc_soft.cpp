cc813b9905a37943eed4f6d07f6519d67dbb541a
AlaskanEmily <emily@alaskanemily.net>
2018-11-21 23:28:42 -0800

    Change the interpreter to use the bytecode backend

--- src/dc_soft.cpp
+++ src/dc_soft.cpp
@@ -4,251 +4,175 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#include "dc_bytecode.hpp"
 #include "dc_backend.h"
 
-#include <stack>
 #include <vector>
 #include <math.h>
+#include <assert.h>
 
-namespace DC {
+// Software backend
+// The build instructions assembly bytecode.
+// Running interprets the bytecode format defined in dc_bytecode.hpp
 
-class Operation {
-public:
-    virtual void run(std::stack<float> &stack, const float *args) const = 0;
-    virtual ~Operation(){}
+struct DC_X_Context {};
+
+// Inheriting like this allows us to implement the Finalize method as passthrough.
+struct DC_X_Calculation : public DC::Bytecode::Bytecode {
+
 };
 
-class Pop : public Operation {
-public:
-    virtual void run(std::stack<float> &stack, const float *args) const {
-        (void)args;
-        stack.pop();
-    }
-};
+struct DC_X_CalculationBuilder : public DC_X_Calculation {
 
-class UnaryOp : public Operation {
-public:
-    virtual void run(std::stack<float> &stack, const float *args) const {
-        (void)args;
-        stack.top() = op(stack.top());
-    }
-    virtual float op(float a) const = 0;
-};
-
-class Sin : public UnaryOp {
-public:
-    virtual float op(float a) const {
-        return static_cast<float>(sin(a));
-    }
-};
-
-class Cos : public UnaryOp {
-public:
-    virtual float op(float a) const {
-        return static_cast<float>(cos(a));
-    }
-};
-
-class Sqrt : public UnaryOp {
-public:
-    virtual float op(float a) const {
-        return static_cast<float>(sqrt(a));
-    }
-};
-
-class BinaryOp : public Operation {
-public:
-    virtual void run(std::stack<float> &stack, const float *args) const {
-        (void)args;
-        const float a = stack.top();
-        stack.pop();
-        stack.top() = op(a, stack.top());
-    }
-    virtual float op(float a, float b) const = 0;
-};
-
-class Add : public BinaryOp {
-public:
-    virtual float op(float a, float b) const {
-        return a + b;
-    }
-};
-
-class Sub : public BinaryOp {
-public:
-    virtual float op(float a, float b) const {
-        return a - b;
-    }
-};
-
-class Div : public BinaryOp {
-public:
-    virtual float op(float a, float b) const {
-        return a / b;
-    }
-};
-
-class Mul : public BinaryOp {
-public:
-    virtual float op(float a, float b) const {
-        return a * b;
-    }
-};
-
-class Term : public Operation {
-public:
-    virtual float evaluate(const float *args) const = 0;
-    virtual void run(std::stack<float> &stack, const float *args) const {
-        stack.push(evaluate(args));
-    }
-};
-
-class Argument : public Term {
-    unsigned short m_arg_num;
-public:
-    Argument(unsigned short arg_num)
-      : m_arg_num(arg_num){}
-    
-    virtual float evaluate(const float *args) const {
-        return args[m_arg_num];
-    }
-};
-
-class Immediate : public Term {
-    float m_immediate;
-public:
-    Immediate(float immediate)
-      : m_immediate(immediate){}
-    
-    virtual float evaluate(const float *args) const {
-        (void)args;
-        return m_immediate;
-    }
-};
-
-} // namespace DC
-
-struct DC_X_Context{};
-
-struct DC_X_Calculation {
-    virtual ~DC_X_Calculation() {}
-    virtual float run(const float *args) const = 0;
-};
-
-struct DC_X_CalculationBuilder : public DC_X_Calculation{
-private:
-    typedef std::vector<DC::Operation*> OperationVector;
-    typedef OperationVector::const_iterator iterator;
-    
-    OperationVector m_operations;
-public:
-    virtual ~DC_X_CalculationBuilder() {
-        for(iterator i = m_operations.begin(); i != m_operations.end(); i++){
-            delete *i;
-        }
-    }
-    float run(const float *args) const {
-        std::stack<float> stack;
-        for(iterator i = m_operations.begin();
-            i != m_operations.end();
-            i++){
-            const DC::Operation &op = **i;
-            op.run(stack, args);
-        }
-        return stack.top();
-    }
-    void push(DC::Operation *op){
-        m_operations.push_back(op);
-    }
-    
-    template<typename T>
-    void push(){
-        DC::Operation *const op = new T();
-        m_operations.push_back(op);
-    }
 };
 
 DC_X_Context *DC_X_CreateContext(void){
-    return new DC_X_Context();
+    return new DC_X_Context;
 }
 
-void DC_X_FreeContext(DC_X_Context *ctx){
+void DC_X_FreeContext(struct DC_X_Context *ctx){
     delete ctx;
 }
 
-DC_X_CalculationBuilder *DC_X_CreateCalculationBuilder(
-    struct DC_X_Context *ctx){
+DC_X_CalculationBuilder *DC_X_CreateCalculationBuilder(DC_X_Context *ctx){
     (void)ctx;
-    return new DC_X_CalculationBuilder();
+    return new DC_X_CalculationBuilder;
 }
 
-void DC_X_BuildPushImmediate(DC_X_Context *ctx,
-    struct DC_X_CalculationBuilder *bld,
-    float value){
+void DC_X_BuildPushImmediate(DC_X_Context *ctx, DC_X_CalculationBuilder *bld, float value){
     (void)ctx;
-    bld->push(new DC::Immediate(value));
+    bld->writeImmediate(value);
 }
 
-void DC_X_BuildPushArg(struct DC_X_Context *ctx,
-    struct DC_X_CalculationBuilder *bld,
-    unsigned short arg_num){
+void DC_X_BuildPushArg(DC_X_Context *ctx, DC_X_CalculationBuilder *bld, unsigned short arg_num){
     (void)ctx;
-    bld->push(new DC::Argument(arg_num));
+    bld->writeArgument(arg_num);
 }
 
-#define DC_X_BUILD(NAME)\
-void DC_X_Build ## NAME(struct DC_X_Context *ctx,\
-    struct DC_X_CalculationBuilder *bld){\
-    (void)ctx;\
-    bld->push<DC::NAME>();\
+#define DC_SOFT_UNOP(NAME)\
+void DC_X_Build ## NAME(DC_X_Context *ctx, DC_X_CalculationBuilder *bld){\
+    (void)ctx; \
+    bld->writeUnary<DC::Bytecode::e ## NAME>(); \
 }\
-void DC_X_Build ## NAME ## Arg(struct DC_X_Context *ctx,\
-    struct DC_X_CalculationBuilder *bld,\
-    unsigned short arg_num){\
-    (void)ctx;\
-    bld->push(new DC::Argument(arg_num));\
-    bld->push<DC::NAME>();\
+void DC_X_Build ## NAME ## Arg(DC_X_Context *ctx, \
+    DC_X_CalculationBuilder *bld, unsigned short arg_num){\
+    (void)ctx; \
+    bld->writeUnaryArgument<DC::Bytecode::e ## NAME>(arg_num); \
 }
 
-DC_X_BUILD(Add)
-DC_X_BUILD(Sub)
-DC_X_BUILD(Div)
-DC_X_BUILD(Mul)
-DC_X_BUILD(Sqrt)
-DC_X_BUILD(Sin)
-DC_X_BUILD(Cos)
-
-#define DC_X_BUILD_IMM(NAME)\
-void DC_X_Build ## NAME ## Imm(struct DC_X_Context *ctx,\
-    struct DC_X_CalculationBuilder *bld,\
-    float imm){\
-    (void)ctx;\
-    bld->push(new DC::Immediate(imm));\
-    bld->push<DC::NAME>();\
+#define DC_SOFT_BINOP(NAME)\
+void DC_X_Build ## NAME(DC_X_Context *ctx, DC_X_CalculationBuilder *bld){\
+    (void)ctx; \
+    bld->writeBinary<DC::Bytecode::e ## NAME>(); \
+} \
+void DC_X_Build ## NAME ## Arg(DC_X_Context *ctx, \
+    DC_X_CalculationBuilder *bld, unsigned short arg_num){\
+    (void)ctx; \
+    bld->writeBinaryArgument<DC::Bytecode::e ## NAME>(arg_num); \
+} \
+void DC_X_Build ## NAME ## Imm(DC_X_Context *ctx, \
+    DC_X_CalculationBuilder *bld, float value){\
+    (void)ctx; \
+    bld->writeBinaryImmediate<DC::Bytecode::e ## NAME>(value); \
 }
 
-DC_X_BUILD_IMM(Add)
-DC_X_BUILD_IMM(Sub)
-DC_X_BUILD_IMM(Div)
-DC_X_BUILD_IMM(Mul)
+void DC_X_BuildPop(DC_X_Context *ctx, DC_X_CalculationBuilder *bld){\
+    (void)ctx;
+    bld->writeUnary<DC::Bytecode::ePop>();
+}
 
-void DC_X_AbandonCalculation(struct DC_X_Context *ctx,
-    struct DC_X_CalculationBuilder *bld){
+DC_SOFT_UNOP(Sin)
+DC_SOFT_UNOP(Cos)
+DC_SOFT_UNOP(Sqrt)
+
+DC_SOFT_BINOP(Add)
+DC_SOFT_BINOP(Sub)
+DC_SOFT_BINOP(Mul)
+DC_SOFT_BINOP(Div)
+
+void DC_X_AbandonCalculation(DC_X_Context *ctx, DC_X_CalculationBuilder *bld){
     (void)ctx;
     delete bld;
 }
 
-struct DC_X_Calculation *DC_X_FinalizeCalculation(struct DC_X_Context *ctx,
-    struct DC_X_CalculationBuilder *bld){
+DC_X_Calculation *DC_X_FinalizeCalculation(DC_X_Context *ctx, DC_X_CalculationBuilder *bld){
     (void)ctx;
     return bld;
 }
 
-void DC_X_Free(struct DC_X_Context *ctx, struct DC_X_Calculation *calc){
+void DC_X_Free(DC_X_Context *ctx, DC_X_Calculation *calc){
     (void)ctx;
     delete calc;
 }
 
 float DC_X_Calculate(const struct DC_X_Calculation *calc, const float *args){
-    return calc->run(args);
+    DC::Bytecode::Bytecode::iterator iter = calc->begin(), end = calc->end();
+    std::vector<float> stack;
+    stack.reserve(16); // Pretty reasonable guess
+    while(iter != end){
+        switch(iter.opType()){
+            case DC::Bytecode::eImmediate:
+                // Push an immediate onto the stack
+                stack.push_back(iter.readImmediate());
+                continue;
+            case DC::Bytecode::eArgument:
+                // Push an argument onto the stack
+                {
+                    const unsigned short arg_num = iter.readArgument();
+                    const float value = args[arg_num];
+                    stack.push_back(value);
+                }
+                continue;
+            case DC::Bytecode::eUnary:
+                // Get the value to operate on.
+                // All unary ops work on the top value of the stack.
+                {
+                    assert(!stack.empty());
+                    const float value = stack.back();
+                    switch(iter.readUnaryOp()){
+                        case DC::Bytecode::eSin:
+                            stack.back() = sin(value);
+                            continue;
+                        case DC::Bytecode::eCos:
+                            stack.back() = cos(value);
+                            continue;
+                        case DC::Bytecode::eSqrt:
+                            stack.back() = sqrt(value);
+                            continue;
+                        case DC::Bytecode::ePop:
+                            stack.pop_back();
+                            continue;
+                    }
+                }
+                assert(NULL == "Invalid unary op.");
+                continue;
+            case DC::Bytecode::eBinary:
+                assert(stack.size() >= 2);
+                {
+                    const float value = stack.back();
+                    stack.pop_back();
+                    switch(iter.readBinaryOp()){
+                        case DC::Bytecode::eAdd:
+                            stack.back() += value;
+                            continue;
+                        case DC::Bytecode::eSub:
+                            stack.back() -= value;
+                            continue;
+                        case DC::Bytecode::eMul:
+                            stack.back() *= value;
+                            continue;
+                        case DC::Bytecode::eDiv:
+                            stack.back() /= value;
+                            continue;
+                    }
+                }
+                assert(NULL == "Invalid binary op.");
+                continue;
+        }
+        assert(NULL == "Invalid op type.");
+        continue;
+    }
+    
+    assert(stack.size() == 1);
+    return stack.back();
 }
